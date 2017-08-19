78e3ee20baceba62933d203726eb606061732e36
Javier Martínez Villacampa <villacampa@gmail.com>
2017-08-20 00:13:59 +0200

    Implemented initial ThingHandlers interface and production objects.

---
+++ src/main/java/info/ciclope/wotgate/things/thing/ProductionThingHandlers.java
@@ -0,0 +1,96 @@
+/*
+ *  Copyright (c) 2017, Javier Martínez Villacampa
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package info.ciclope.wotgate.things.thing;
+
+import info.ciclope.wotgate.storage.database.DatabaseStorage;
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+
+public class ProductionThingHandlers implements ThingHandlers {
+    private ThingHandlerRegister handlerRegister;
+    private DatabaseStorage databaseStorage;
+
+    public ProductionThingHandlers(ThingHandlerRegister register, DatabaseStorage storage) {
+        this.handlerRegister = register;
+        this.databaseStorage = storage;
+    }
+
+    @Override
+    public void getThingConfiguration(Message<Void> message) {
+        message.reply(handlerRegister.getThingConfiguration());
+    }
+
+    @Override
+    public void provideThingDescription(Message<Void> message) {
+        message.reply(handlerRegister.getThingDescription().getDescription());
+    }
+
+    @Override
+    public void getThingDescription(Message<JsonObject> message) {
+        message.reply(handlerRegister.getThingDescription().getDescription());
+    }
+
+    @Override
+    public void setThingDescription(Message<JsonObject> message) {
+        ThingRequest request = new ThingRequest(message.body());
+        handlerRegister.setThingDescription(new ThingDescription(request.getBody()));
+    }
+
+    @Override
+    public void getThingProperty(Message<JsonObject> message) {
+        if (handlerRegister.containsAddressHandler(message.address())) {
+            handlerRegister.getAddressHandler(message.address()).handle(message);
+        } else {
+            return;
+        }
+    }
+
+    @Override
+    public void postThingProperty(Message<JsonObject> message) {
+
+    }
+
+    @Override
+    public void putThingProperty(Message<JsonObject> message) {
+
+    }
+
+    @Override
+    public void getThingArrayPropertyElement(Message<JsonObject> message) {
+
+    }
+
+    @Override
+    public void putThingArrayPropertyElement(Message<JsonObject> message) {
+
+    }
+
+    @Override
+    public void deleteThingArrayPropertyElement(Message<JsonObject> message) {
+
+    }
+
+    @Override
+    public void getThingAction(Message<JsonObject> message) {
+
+    }
+
+    @Override
+    public void postThingAction(Message<JsonObject> message) {
+
+    }
+}
---
+++ src/main/java/info/ciclope/wotgate/things/thing/ThingHandlers.java
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (c) 2017, Javier Martínez Villacampa
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package info.ciclope.wotgate.things.thing;
+
+import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonObject;
+
+public interface ThingHandlers {
+
+    void getThingConfiguration(Message<Void> message);
+
+    void provideThingDescription(Message<Void> message);
+
+    void getThingDescription(Message<JsonObject> message);
+
+    void setThingDescription(Message<JsonObject> message);
+
+    void getThingProperty(Message<JsonObject> message);
+
+    void postThingProperty(Message<JsonObject> message);
+
+    void putThingProperty(Message<JsonObject> message);
+
+    void getThingArrayPropertyElement(Message<JsonObject> message);
+
+    void putThingArrayPropertyElement(Message<JsonObject> message);
+
+    void deleteThingArrayPropertyElement(Message<JsonObject> message);
+
+    void getThingAction(Message<JsonObject> message);
+
+    void postThingAction(Message<JsonObject> message);
+
+}
