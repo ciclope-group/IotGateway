e57656d50b7a6aa9dedfe04d1a65c3bdf21ae462
Javier Martínez Villacampa <villacampa@gmail.com>
2017-08-21 22:50:02 +0200

    Added PasswordManager for the gatekeeper.

---
+++ src/main/java/info/ciclope/wotgate/thing/driver/gatekeeper/authorizer/PasswordManager.java
@@ -0,0 +1,106 @@
+/*
+ *  Copyright (c) 2017, Javier Martínez Villacampa
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package info.ciclope.wotgate.thing.driver.gatekeeper.authorizer;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Hash passwords for storage, and test passwords against password tokens.
+ * <p>
+ * Instances of this class can be used concurrently by multiple threads.
+ *
+ * @author erickson
+ * @see <a href="http://stackoverflow.com/a/2861125/3474">StackOverflow</a>
+ */
+public final class PasswordManager {
+
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA512";
+    private static final int SIZE = 128;
+    private static final Pattern layout = Pattern.compile("(\\d\\d?)\\$(.{43})");
+    private static final int cost = 16;
+    private final SecureRandom random;
+
+    /**
+     * Create a password manager with a specified cost
+     */
+    public PasswordManager() {
+        iterations(cost); /* Validate cost */
+        this.random = new SecureRandom();
+    }
+
+    private static int iterations(int cost) {
+        if ((cost & ~0x1E) != 0)
+            throw new IllegalArgumentException("cost: " + cost);
+        return 1 << cost;
+    }
+
+    private static byte[] pbkdf2(char[] password, byte[] salt, int iterations) {
+        KeySpec spec = new PBEKeySpec(password, salt, iterations, SIZE);
+        try {
+            SecretKeyFactory f = SecretKeyFactory.getInstance(ALGORITHM);
+            return f.generateSecret(spec).getEncoded();
+        } catch (NoSuchAlgorithmException ex) {
+            throw new IllegalStateException("Missing algorithm: " + ALGORITHM, ex);
+        } catch (InvalidKeySpecException ex) {
+            throw new IllegalStateException("Invalid SecretKeyFactory", ex);
+        }
+    }
+
+    /**
+     * Hash a password for storage.
+     *
+     * @return a secure authentication token to be stored for later authentication
+     */
+    public String hash(char[] password) {
+        byte[] salt = new byte[SIZE / 8];
+        random.nextBytes(salt);
+        byte[] dk = pbkdf2(password, salt, 1 << cost);
+        byte[] hash = new byte[salt.length + dk.length];
+        System.arraycopy(salt, 0, hash, 0, salt.length);
+        System.arraycopy(dk, 0, hash, salt.length, dk.length);
+        Base64.Encoder enc = Base64.getUrlEncoder().withoutPadding();
+        return cost + "$" + enc.encodeToString(hash);
+    }
+
+    /**
+     * Authenticate with a password and a stored password token.
+     *
+     * @return true if the password and token match
+     */
+    public boolean authenticate(char[] password, String token) {
+        Matcher m = layout.matcher(token);
+        if (!m.matches())
+            throw new IllegalArgumentException("Invalid token format");
+        int iterations = iterations(Integer.parseInt(m.group(1)));
+        byte[] hash = Base64.getUrlDecoder().decode(m.group(2));
+        byte[] salt = Arrays.copyOfRange(hash, 0, SIZE / 8);
+        byte[] check = pbkdf2(password, salt, iterations);
+        int zero = 0;
+        for (int idx = 0; idx < check.length; ++idx)
+            zero |= hash[salt.length + idx] ^ check[idx];
+        return zero == 0;
+    }
+}
